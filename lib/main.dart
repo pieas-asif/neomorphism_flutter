519a9f765bee38a8e02dc3f3ed5a0272e09193d1
Mehdi <mehdi@evidentbd.com>
2021-11-05 10:12:17 +0600

    Gradient Added

--- lib/main.dart
+++ lib/main.dart
@@ -34,21 +34,35 @@ class _HomeState extends State<Home> {
         width: 200,
         decoration: BoxDecoration(
           color: Colors.grey[300],
-          borderRadius: BorderRadius.circular(50),
-          boxShadow: const [
+          shape: BoxShape.circle,
+          boxShadow: [
             BoxShadow(
-              color: Colors.grey,
-              offset: Offset(4.0, 4.0),
+              color: Colors.grey.shade600,
+              offset: const Offset(4.0, 4.0),
               blurRadius: 15.0,
               spreadRadius: 1.0,
             ),
-            BoxShadow(
+            const BoxShadow(
               color: Colors.white,
               offset: Offset(-4.0, -4.0),
               blurRadius: 15.0,
               spreadRadius: 1.0,
             ),
-          ]
+            
+          ],
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey.shade200,
+              Colors.grey.shade300,
+              Colors.grey.shade400,
+              Colors.grey.shade500,
+            ], 
+            stops: const [
+              0.1, 0.3, 0.8, 1,
+            ]
+          )
         ),
         child: const Icon(Icons.android, size: 80,),
       ),),
